46e0a5025679dd4af03678b61051e08c875a660b
madelmann <madelmann@61037b34-3826-4e90-a829-d4c060d010e7>
2015-12-19 10:57:11 +0000

    fixed convertion from string to boolean => now all tests work on Mac OS X

--- trunk/Projects/Core/Core/AST/Assign.cpp
+++ trunk/Projects/Core/Core/AST/Assign.cpp
@@ -26,21 +26,21 @@ Node* Assign::execute()
 	mInterpreter->setScope(mScope);
 	mInterpreter->setTokens(mTokens);
 
-	Object *assignee = mMemory->getObject(mAssignee);
+	Object *assignee = 0;//mMemory->getObject(mAssignee);
 	if ( !assignee ) {
 		// ups..
-		throw Utils::NullPointerException("while trying to access " + mAssignee, (*mTokens.begin()).position());
+		throw Utils::Exceptions::NullPointer("while trying to access " + mAssignee, (*mTokens.begin()).position());
 	}
 
 	// find out if this object is const
 	if ( assignee->isConst() ) {
-		throw Utils::ConstCorrectnessViolated("Not allowed to assign value to const object!", (*mTokens.begin()).position());
+		throw Utils::Exceptions::ConstCorrectnessViolated("Not allowed to assign value to const object!", (*mTokens.begin()).position());
 	}
 
 	// TODO: find out if we are changing a member in a const method
 
 
-	assignee->assign(mInterpreter->process());
+	//assignee->assign(mInterpreter->process());
 
 	return 0;
 }
