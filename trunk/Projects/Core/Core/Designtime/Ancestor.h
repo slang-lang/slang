b864b6ee639723062f65b0f215019d51ab28e829
madelmann <madelmann@61037b34-3826-4e90-a829-d4c060d010e7>
2016-02-21 11:12:04 +0000

    renamed Anchestor to Ancestor BluePrints now store their ancestors (should be only one at a time but is capable of storing more than one) and their implementations (can be more than one at a time) added fixture for inheritance related (unit) tests

--- trunk/Projects/Core/Core/Designtime/Ancestor.h
+++ trunk/Projects/Core/Core/Designtime/Ancestor.h
@@ -1,6 +1,6 @@
 
-#ifndef ObjectiveScript_Designtime_Anchestor_h
-#define ObjectiveScript_Designtime_Anchestor_h
+#ifndef ObjectiveScript_Designtime_Ancestor_h
+#define ObjectiveScript_Designtime_Ancestor_h
 
 
 // Library includes
@@ -22,17 +22,29 @@ namespace Designtime {
 class Ancestor
 {
 public:
+	class Type
+	{
+	public:
+		enum E {
+			Unknown,
+			Extends,
+			Implements
+		};
+	};
+
 	Ancestor();
-	Ancestor(const std::string& name, Visibility::E visibility);
+	Ancestor(const std::string& name, Type::E type, Visibility::E visibility);
 
 public:
 	const std::string& name() const;
+	Type::E type() const;
 	Visibility::E visibility() const;
 
 protected:
 
 private:
 	std::string	mName;
+	Type::E mType;
 	Visibility::E mVisibility;
 };
 
---
+++ trunk/Projects/TestSuite/Inheritance/InheritanceTest.h
@@ -0,0 +1,42 @@
+
+#ifndef Testing_Inheritance_InheritanceTest_h
+#define Testing_Inheritance_InheritanceTest_h
+
+
+// Library includes
+
+// Project includes
+#include <Common/Logger.h>
+#include <Framework/GenericTest.h>
+
+// Forward declarations
+
+// Namespace declarations
+
+
+namespace Testing {
+namespace Inheritance {
+
+
+class InheritanceTest : private Utils::Common::Logger,
+                        public GenericTest
+{
+public:
+    InheritanceTest(const Utils::Common::ILogger *p);
+    ~InheritanceTest();
+
+public:
+    void process();
+    void setup();
+    void teardown();
+
+private:	// Tests
+    void testBasicInheritance();
+};
+
+
+}
+}
+
+
+#endif
