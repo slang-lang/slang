9c9055bda1082ebbaa81ffbf66f66116e6588f76
madelmann <madelmann@61037b34-3826-4e90-a829-d4c060d010e7>
2016-06-19 15:53:14 +0000

    incremented version number

--- trunk/Projects/odebugger/Application/Application.cpp
+++ trunk/Projects/odebugger/Application/Application.cpp
@@ -79,7 +79,7 @@ void Application::printUsage()
 
 void Application::printVersion()
 {
-	std::cout << "ObjectiveScript Debugger 0.3.3 (cli)" << std::endl;
+	std::cout << "ObjectiveScript Debugger 0.3.4 (cli)" << std::endl;
 	std::cout << "Copyright (c) 2014-2016 Michael Adelmann" << std::endl;
 	std::cout << "" << std::endl;
 }
