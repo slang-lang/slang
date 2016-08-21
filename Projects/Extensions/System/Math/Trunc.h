58aab4721cf08df93281af88eee465390671d960
madelmann <madelmann@61037b34-3826-4e90-a829-d4c060d010e7>
2016-08-21 17:28:39 +0000

    added new math extension trunc with unit test

---
+++ Projects/Extensions/System/Math/Trunc.h
@@ -0,0 +1,112 @@
+
+#ifndef ObjectiveScript_Extensions_System_Math_Trunc_h
+#define ObjectiveScript_Extensions_System_Math_Trunc_h
+
+
+// Library includes
+#include <math.h>
+
+// Project includes
+#include <Core/BuildInObjects/DoubleObject.h>
+#include <Core/BuildInObjects/FloatObject.h>
+#include <Core/Designtime/BuildInTypes/DoubleObject.h>
+#include <Core/Designtime/BuildInTypes/FloatObject.h>
+#include <Core/Method.h>
+#include <Core/Repository.h>
+#include <Core/Tools.h>
+#include <Core/Utils/Exceptions.h>
+#include <Tools/Strings.h>
+#include "Math.h"
+
+// Forward declarations
+
+// Namespace declarations
+
+
+namespace ObjectiveScript {
+namespace Extensions {
+namespace System {
+namespace Math {
+
+
+class TruncDouble: public Runtime::Method
+{
+public:
+	TruncDouble()
+	: Runtime::Method(0, "trunc", Designtime::DoubleObject::TYPENAME)
+	{
+		ParameterList params;
+		params.push_back(Parameter("value", Designtime::DoubleObject::TYPENAME, VALUE_NONE));
+
+		setSignature(params);
+	}
+
+public:
+	Runtime::ControlFlow::E execute(const ParameterList& params, Runtime::Object* result, const Token& token)
+	{
+		ParameterList list = mergeParameters(params);
+
+		try {
+			ParameterList::const_iterator it = list.begin();
+
+			double param_value = (*it++).value().toDouble();
+
+			*result = Runtime::DoubleObject(trunc(param_value));
+		}
+		catch ( std::exception& e ) {
+			Runtime::Object *data = mRepository->createInstance(Runtime::StringObject::TYPENAME, ANONYMOUS_OBJECT);
+			*data = Runtime::StringObject(std::string(e.what()));
+
+			mExceptionData = Runtime::ExceptionData(data, token.position());
+			return Runtime::ControlFlow::Throw;
+		}
+
+		return Runtime::ControlFlow::Normal;
+	}
+};
+
+
+class TruncFloat: public Runtime::Method
+{
+public:
+	TruncFloat()
+	: Runtime::Method(0, "trunc", Designtime::FloatObject::TYPENAME)
+	{
+		ParameterList params;
+		params.push_back(Parameter("value", Designtime::FloatObject::TYPENAME, VALUE_NONE));
+
+		setSignature(params);
+	}
+
+public:
+	Runtime::ControlFlow::E execute(const ParameterList& params, Runtime::Object* result, const Token& token)
+	{
+		ParameterList list = mergeParameters(params);
+
+		try {
+			ParameterList::const_iterator it = list.begin();
+
+			double param_value = (*it++).value().toDouble();
+
+			*result = Runtime::DoubleObject(trunc(param_value));
+		}
+		catch ( std::exception& e ) {
+			Runtime::Object *data = mRepository->createInstance(Runtime::StringObject::TYPENAME, ANONYMOUS_OBJECT);
+			*data = Runtime::StringObject(std::string(e.what()));
+
+			mExceptionData = Runtime::ExceptionData(data, token.position());
+			return Runtime::ControlFlow::Throw;
+		}
+
+		return Runtime::ControlFlow::Normal;
+	}
+};
+
+
+}
+}
+}
+}
+
+
+#endif
