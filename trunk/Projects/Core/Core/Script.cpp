f7f1f71b14dcb7401cdac5843c1626f3f44f50f8
madelmann <madelmann@61037b34-3826-4e90-a829-d4c060d010e7>
2016-08-31 10:36:59 +0000

    added extension method pre to apache extension

--- trunk/Projects/Core/Core/Script.cpp
+++ trunk/Projects/Core/Core/Script.cpp
@@ -9,7 +9,7 @@
 #include <Core/Utils/Exceptions.h>
 #include <Core/Utils/Utils.h>
 #include "Object.h"
-#include "Repository.h"
+#include "Scope.h"
 
 // Namespace declarations
 
@@ -17,8 +17,8 @@
 namespace ObjectiveScript {
 
 
-Script::Script(Repository *repository)
-: mRepository(repository)
+Script::Script(MethodScope* scope)
+: mScope(scope)
 {
 }
 
@@ -28,7 +28,7 @@ Script::~Script()
 
 void Script::execute(const std::string& method, const ParameterList& params, Runtime::Object* result)
 {
-	MethodSymbol *symbol = mRepository->getGlobalScope()->resolveMethod(method, params, false);
+	MethodSymbol *symbol = mScope->resolveMethod(method, params, false);
 	if ( !symbol ) {
 		throw Utils::Exceptions::Exception("could not resolve method '" + method + "(" + toString(params) + ")'");
 	}
@@ -37,18 +37,24 @@ void Script::execute(const std::string& method, const ParameterList& params, Run
 	Runtime::ControlFlow::E controlflow = methodSymbol->execute(params, result, Token());
 
 	if ( controlflow == Runtime::ControlFlow::Throw ) {
-		throw Utils::Exceptions::Exception("exception raised in method '" + method + "(" + toString(params) + ")'");
+		Runtime::Object* data = methodSymbol->getExceptionData().getData();
+		//Utils::Position position = methodSymbol->getExceptionData().getPosition();
+
+		std::string text = "exception raised in method '" + method + "(" + toString(params) + ")':\n";
+					text += data->getValue().toStdString();
+
+		throw Utils::Exceptions::Exception(text);
 	}
 }
 
 Symbol* Script::resolve(const std::string &symbol)
 {
-	return mRepository->getGlobalScope()->resolve(symbol);
+	return mScope->resolve(symbol);
 }
 
 Symbol* Script::resolveMethod(const std::string &method, const ParameterList &params)
 {
-	return mRepository->getGlobalScope()->resolveMethod(method, params);
+	return mScope->resolveMethod(method, params);
 }
 
 
