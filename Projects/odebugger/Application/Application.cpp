4397d6a3e00253160490dc1147fb3a0ff5c900dc
madelmann <madelmann@61037b34-3826-4e90-a829-d4c060d010e7>
2016-11-13 16:06:29 +0000

    updated version numbers and README

--- Projects/odebugger/Application/Application.cpp
+++ Projects/odebugger/Application/Application.cpp
@@ -97,7 +97,7 @@ void Application::printUsage()
 
 void Application::printVersion()
 {
-	std::cout << "ObjectiveScript Debugger 0.5.1 (cli)" << std::endl;
+	std::cout << "ObjectiveScript Debugger 0.5.2 (cli)" << std::endl;
 	std::cout << "Copyright (c) 2014-2016 Michael Adelmann" << std::endl;
 	std::cout << "" << std::endl;
 }
