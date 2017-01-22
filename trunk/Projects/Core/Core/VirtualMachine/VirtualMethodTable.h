dbb29366eec57c6f76ebe66949e265ea5edd6b0c
madelmann <madelmann@61037b34-3826-4e90-a829-d4c060d010e7>
2017-01-22 21:39:04 +0000

    made buildable in Visual Studio 2008

--- trunk/Projects/Core/Core/VirtualMachine/VirtualMethodTable.h
+++ trunk/Projects/Core/Core/VirtualMachine/VirtualMethodTable.h
@@ -6,6 +6,7 @@
 // Library includes
 #include <list>
 #include <map>
+//#include <unordered_map>
 
 // Project includes
 #include <Core/Parameter.h>
@@ -48,6 +49,7 @@ private:
 
 	typedef std::list<MethodAndParent> MethodCollection;
 	typedef std::map<std::string /*parent*/, MethodCollection> MethodTable;
+	//typedef std::unordered_map<std::string /*parent*/, MethodCollection> MethodTable;
 
 private:
 	MethodTable mMethods;
