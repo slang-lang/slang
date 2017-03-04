f370e88d2da771b24ef76a90c13b7e486772e84a
madelmann <madelmann@61037b34-3826-4e90-a829-d4c060d010e7>
2017-03-04 18:49:46 +0000

    AST: added basic type operations to type system

--- trunk/Projects/Core/Core/VirtualMachine/TypeSystem.cpp
+++ trunk/Projects/Core/Core/VirtualMachine/TypeSystem.cpp
@@ -5,6 +5,7 @@
 // Library includes
 
 // Project includes
+#include <Core/Common/Exceptions.h>
 #include <Core/Designtime/BuildInTypes/BoolObject.h>
 #include <Core/Designtime/BuildInTypes/DoubleObject.h>
 #include <Core/Designtime/BuildInTypes/FloatObject.h>
@@ -12,6 +13,7 @@
 #include <Core/Designtime/BuildInTypes/IntegerObject.h>
 #include <Core/Designtime/BuildInTypes/StringObject.h>
 #include <Core/Designtime/BuildInTypes/VoidObject.h>
+#include <Core/Designtime/Exceptions.h>
 
 // Namespace declarations
 using namespace ObjectiveScript::Designtime;
@@ -30,10 +32,20 @@ TypeSystem::~TypeSystem()
 
 void TypeSystem::define(const std::string& left, Token::Type::E operation, const std::string& right, const std::string& result)
 {
-	(void)left;
-	(void)operation;
-	(void)result;
-	(void)right;
+	if ( !mTypeMap.contains(left) ) {
+		mTypeMap.insert(left);
+	}
+
+	if ( !mTypeMap[left].contains(operation) ) {
+		mTypeMap[left].insert(operation);
+	}
+
+	if ( mTypeMap[left][operation].contains(right) ) {
+		throw Common::Exceptions::Exception("duplicate type system target");
+	}
+
+	mTypeMap[left][operation].insert(right);
+	mTypeMap[left][operation][right] = result;
 }
 
 void TypeSystem::deinit()
@@ -41,23 +53,48 @@ void TypeSystem::deinit()
 	// nothing to do here atm
 }
 
-std::string TypeSystem::getType(const std::string& left, Token::Type::E operation, const std::string& right) const
+std::string TypeSystem::getType(const std::string& left, Token::Type::E operation, const std::string& right)
 {
-	(void)left;
-	(void)operation;
-	(void)right;
+	if ( !mTypeMap.contains(left) ) {
+		throw Common::Exceptions::NotSupported(left);
+	}
+	if ( !mTypeMap[left].contains(operation) ) {
+		throw Common::Exceptions::NotSupported(left);
+	}
+	if ( !mTypeMap[left][operation].contains(right) ) {
+		throw Common::Exceptions::NotSupported(left + " with " + right);
+	}
 
-	return "";
+	return mTypeMap[left][operation][right];
 }
 
 void TypeSystem::init()
 {
+	// bool
+	define(BoolObject::TYPENAME, Token::Type::COMPARE_EQUAL,   BoolObject::TYPENAME, BoolObject::TYPENAME);
+	define(BoolObject::TYPENAME, Token::Type::COMPARE_UNEQUAL, BoolObject::TYPENAME, BoolObject::TYPENAME);
+
+	// double
+	define(DoubleObject::TYPENAME, Token::Type::MATH_ADDITION, DoubleObject::TYPENAME, DoubleObject::TYPENAME);
+	define(DoubleObject::TYPENAME, Token::Type::MATH_DIVIDE,   DoubleObject::TYPENAME, DoubleObject::TYPENAME);
+	define(DoubleObject::TYPENAME, Token::Type::MATH_MULTIPLY, DoubleObject::TYPENAME, DoubleObject::TYPENAME);
+	define(DoubleObject::TYPENAME, Token::Type::MATH_SUBTRACT, DoubleObject::TYPENAME, DoubleObject::TYPENAME);
+
+	// float
+	define(FloatObject::TYPENAME, Token::Type::MATH_ADDITION, FloatObject::TYPENAME, FloatObject::TYPENAME);
+	define(FloatObject::TYPENAME, Token::Type::MATH_DIVIDE,   FloatObject::TYPENAME, FloatObject::TYPENAME);
+	define(FloatObject::TYPENAME, Token::Type::MATH_MULTIPLY, FloatObject::TYPENAME, FloatObject::TYPENAME);
+	define(FloatObject::TYPENAME, Token::Type::MATH_SUBTRACT, FloatObject::TYPENAME, FloatObject::TYPENAME);
+
 	// int
 	define(IntegerObject::TYPENAME, Token::Type::MATH_ADDITION, IntegerObject::TYPENAME, IntegerObject::TYPENAME);
-	define(IntegerObject::TYPENAME, Token::Type::MATH_DIVIDE, IntegerObject::TYPENAME, IntegerObject::TYPENAME);
-	define(IntegerObject::TYPENAME, Token::Type::MATH_MODULO, IntegerObject::TYPENAME, IntegerObject::TYPENAME);
+	define(IntegerObject::TYPENAME, Token::Type::MATH_DIVIDE,   IntegerObject::TYPENAME, IntegerObject::TYPENAME);
+	define(IntegerObject::TYPENAME, Token::Type::MATH_MODULO,   IntegerObject::TYPENAME, IntegerObject::TYPENAME);
 	define(IntegerObject::TYPENAME, Token::Type::MATH_MULTIPLY, IntegerObject::TYPENAME, IntegerObject::TYPENAME);
 	define(IntegerObject::TYPENAME, Token::Type::MATH_SUBTRACT, IntegerObject::TYPENAME, IntegerObject::TYPENAME);
+
+	// string
+	define(StringObject::TYPENAME, Token::Type::MATH_ADDITION, StringObject::TYPENAME, StringObject::TYPENAME);
 }
 
 
