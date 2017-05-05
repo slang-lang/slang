6102421824d706c88b128941114111d6cf936d4c
madelmann <madelmann@61037b34-3826-4e90-a829-d4c060d010e7>
2017-05-05 13:22:43 +0000

    odepend: linked curl into project

--- branches/AST_Bugfix/Projects/odepend/main.cpp
+++ branches/AST_Bugfix/Projects/odepend/main.cpp
@@ -2,10 +2,17 @@
 // Library includes
 #include <set>
 #include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <curl/curl.h>
 
 // Project includes
 #include <Common/StdOutLogger.h>
+#include <Core/Types.h>
 #include <Tools/Strings.h>
+#include <Utils.h>
 
 // Namespace declarations
 
@@ -32,9 +39,59 @@
 Utils::Common::StdOutLogger mLogger;
 std::set<std::string> mOutdatedModules;
 
+bool download(const std::string& url, const std::string& target);
 void update();
 void upgrade();
 
+static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
+{
+	size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
+	return written;
+}
+
+bool download(const std::string& url, const std::string& target)
+{
+	CURL *curl_handle;
+	FILE *pagefile;
+	bool result = false;
+
+	curl_global_init(CURL_GLOBAL_ALL);
+
+	/* init the curl session */
+	curl_handle = curl_easy_init();
+
+	/* set URL to get here */
+	curl_easy_setopt(curl_handle, CURLOPT_URL, url.c_str());
+
+	/* Switch on full protocol/debug output while testing */
+	curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
+
+	/* disable progress meter, set to 0L to enable and disable debug output */
+	curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
+
+	/* send all data to this function  */
+	curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
+
+	/* open the file */
+	pagefile = fopen(target.c_str(), "wb");
+	if ( pagefile ) {
+		/* write the page body to this file handle */
+		curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, pagefile);
+
+		/* get it! */
+		curl_easy_perform(curl_handle);
+
+		/* close the header file */
+		fclose(pagefile);
+
+		result = true;
+	}
+
+	/* cleanup curl stuff */
+	curl_easy_cleanup(curl_handle);
+
+	return result;
+}
 
 void printUsage()
 {
@@ -43,10 +100,11 @@ void printUsage()
 	std::cout << "help                       This help" << std::endl;
 	std::cout << "install                    Install new module" << std::endl;
 	std::cout << "list                       List all available modules" << std::endl;
+	std::cout << "remove                     Remove an installed module" << std::endl;
 	std::cout << "search                     Search for a module" << std::endl;
 	std::cout << "update                     Update repository indices" << std::endl;
 	std::cout << "upgrade                    Upgrade outdated modules" << std::endl;
-	std::cout << "--version                  Version information" << std::endl;
+	std::cout << "version                    Version information" << std::endl;
 	std::cout << std::endl;
 }
 
@@ -59,20 +117,34 @@ void printVersion()
 
 void processParameters(int argc, const char* argv[])
 {
+	StringList params;
+
 	if ( argc > 1 ) {
 		for ( int i = 1; i < argc; i++ ) {
-			if ( Utils::Tools::StringCompare(argv[i], "update") ) {
+			if ( Utils::Tools::StringCompare(argv[i], "help") ) {
+				printUsage();
+
+				exit(0);
+			}
+			else if ( Utils::Tools::StringCompare(argv[i], "install") ) {
+				// TODO: install
+			}
+			else if ( Utils::Tools::StringCompare(argv[i], "list") ) {
+				// TODO: list
+			}
+			else if ( Utils::Tools::StringCompare(argv[i], "remove") ) {
+				// TODO: remove
+			}
+			else if ( Utils::Tools::StringCompare(argv[i], "search") ) {
+				// TODO: search
+			}
+			else if ( Utils::Tools::StringCompare(argv[i], "update") ) {
 				return update();
 			}
 			else if ( Utils::Tools::StringCompare(argv[i], "upgrade") ) {
 				return upgrade();
 			}
-			else if ( Utils::Tools::StringCompare(argv[i], "-h") || Utils::Tools::StringCompare(argv[i], "--help") ) {
-				printUsage();
-
-				exit(0);
-			}
-			else if ( Utils::Tools::StringCompare(argv[i], "--version") ) {
+			else if ( Utils::Tools::StringCompare(argv[i], "version") ) {
 				printVersion();
 
 				exit(0);
@@ -96,6 +168,11 @@ void update()
 	// (3) list all outdated modules
 
 	std::cout << "Updating..." << std::endl;
+
+	bool result = download("https://michaeladelmann.ticketsharing.net/repo/stable/index/index.json", "index.json");
+	if ( result ) {
+		std::cout << "Updated index successfully." << std::endl;
+	}
 }
 
 void upgrade()
