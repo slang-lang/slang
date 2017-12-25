d5e6e33cbfeccff38d914719b7fc25edf3f184a2
madelmann <madelmann@61037b34-3826-4e90-a829-d4c060d010e7>
2017-12-25 13:46:16 +0000

    increased version number to 0.6.0

--- trunk/Projects/odebugger/Application/Application.cpp
+++ trunk/Projects/odebugger/Application/Application.cpp
@@ -97,7 +97,7 @@ void Application::printUsage()
 
 void Application::printVersion()
 {
-	std::cout << "ObjectiveScript Debugger 0.5.7 (cli)" << std::endl;
+	std::cout << "ObjectiveScript Debugger 0.6.0 (cli)" << std::endl;
 	std::cout << "Copyright (c) 2014-2017 Michael Adelmann" << std::endl;
 	std::cout << "" << std::endl;
 }
