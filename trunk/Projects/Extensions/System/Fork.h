6cfc37d0cbf778265400c5f67949e2615e82d2ad
madelmann <madelmann@61037b34-3826-4e90-a829-d4c060d010e7>
2018-12-26 10:52:17 +0000

    Extensions: added fork method

---
+++ trunk/Projects/Extensions/System/Fork.h
@@ -0,0 +1,64 @@
+
+#ifndef ObjectiveScript_Extensions_System_Fork_h
+#define ObjectiveScript_Extensions_System_Fork_h
+
+
+// Library includes
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+// Project includes
+#include <Core/Common/Exceptions.h>
+#include <Core/Designtime/BuildInTypes/StringObject.h>
+#include <Core/Extensions/ExtensionMethod.h>
+#include <Core/Runtime/BuildInTypes/StringObject.h>
+#include <Core/Tools.h>
+#include <Tools/Strings.h>
+
+// Forward declarations
+
+// Namespace declarations
+
+
+namespace ObjectiveScript {
+namespace Extensions {
+namespace System {
+
+
+class Fork : public ExtensionMethod
+{
+public:
+	Fork()
+	: ExtensionMethod(0, "fork", Designtime::VoidObject::TYPENAME)
+	{
+		ParameterList params;
+
+		setSignature(params);
+	}
+
+public:
+	Runtime::ControlFlow::E execute(Common::ThreadId threadId, const ParameterList& /*params*/, Runtime::Object* /*result*/, const Token& token)
+	{
+		try {
+			fork();
+		}
+		catch ( std::exception& e ) {
+			Runtime::Object *data = Controller::Instance().repository()->createInstance(Runtime::StringObject::TYPENAME, ANONYMOUS_OBJECT);
+			*data = Runtime::StringObject(std::string(e.what()));
+
+			Controller::Instance().thread(threadId)->exception() = Runtime::ExceptionData(data, token.position());
+			return Runtime::ControlFlow::Throw;
+		}
+
+		return Runtime::ControlFlow::Normal;
+	}
+};
+
+
+}
+}
+}
+
+
+#endif
