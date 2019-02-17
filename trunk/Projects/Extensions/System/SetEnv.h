16b922c6f0da882dd2a5a1c26439ab2d086aea40
madelmann <madelmann@61037b34-3826-4e90-a829-d4c060d010e7>
2019-02-17 11:39:39 +0000

    added POSIX.1-2001, POSIX.1-2008, 4.3BSD conforming setenv method to set environment variables

---
+++ trunk/Projects/Extensions/System/SetEnv.h
@@ -0,0 +1,74 @@
+
+#ifndef ObjectiveScript_Extensions_System_SetEnv_h
+#define ObjectiveScript_Extensions_System_SetEnv_h
+
+
+// Library includes
+#include <stdlib.h>
+
+// Project includes
+#include <Core/Common/Exceptions.h>
+#include <Core/Designtime/BuildInTypes/IntegerObject.h>
+#include <Core/Designtime/BuildInTypes/StringObject.h>
+#include <Core/Extensions/ExtensionMethod.h>
+#include <Core/Runtime/BuildInTypes/StringObject.h>
+#include <Core/Tools.h>
+#include <Tools/Strings.h>
+
+// Forward declarations
+
+// Namespace declarations
+
+
+namespace ObjectiveScript {
+namespace Extensions {
+namespace System {
+
+
+class SetEnv : public ExtensionMethod
+{
+public:
+	SetEnv()
+	: ExtensionMethod(0, "setenv", Designtime::IntegerObject::TYPENAME)
+	{
+		ParameterList params;
+		params.push_back(Parameter::CreateDesigntime("name", Designtime::StringObject::TYPENAME));
+		params.push_back(Parameter::CreateDesigntime("value", Designtime::StringObject::TYPENAME));
+		params.push_back(Parameter::CreateDesigntime("overwrite", Designtime::IntegerObject::TYPENAME));
+
+		setSignature(params);
+	}
+
+public:
+	Runtime::ControlFlow::E execute(Common::ThreadId threadId, const ParameterList& params, Runtime::Object* result, const Token& token)
+	{
+		ParameterList list = mergeParameters(params);
+
+		try {
+			ParameterList::const_iterator it = list.begin();
+
+			std::string param_name = (*it++).value().toStdString();
+			std::string param_value = (*it++).value().toStdString();
+			int param_overwrite = (*it++).value().toInt();
+
+			*result = Runtime::IntegerObject(setenv(param_name.c_str(), param_value.c_str(), param_overwrite));
+		}
+		catch ( std::exception& e ) {
+			Runtime::Object *data = Controller::Instance().repository()->createInstance(Runtime::StringObject::TYPENAME, ANONYMOUS_OBJECT);
+			*data = Runtime::StringObject(std::string(e.what()));
+
+			Controller::Instance().thread(threadId)->exception() = Runtime::ExceptionData(data, token.position());
+			return Runtime::ControlFlow::Throw;
+		}
+
+		return Runtime::ControlFlow::Normal;
+	}
+};
+
+
+}
+}
+}
+
+
+#endif
