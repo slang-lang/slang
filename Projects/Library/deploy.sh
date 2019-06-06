6902797629454f8035b1f839cb9481e5e44c0721
madelmann <madelmann@61037b34-3826-4e90-a829-d4c060d010e7>
2019-06-06 20:00:59 +0000

    added support for repos other than 'stable'

--- Projects/Library/deploy.sh
+++ Projects/Library/deploy.sh
@@ -4,9 +4,14 @@ if [ x$1 != "x" ]; then
 	odepend create $1
 fi
 
-scp *.json ticketsharing:/home/ubuntu/projects/objectivescript/repo/stable/modules/
+REPO="stable"
+if [ x$2 != "x" ]; then
+	REPO="$2"
+fi
+
+scp *.json ticketsharing:/home/ubuntu/projects/objectivescript/repo/${REPO}/modules/
 rm *.json
 
-scp *.tar.gz ticketsharing:/home/ubuntu/projects/objectivescript/repo/stable/modules/
+scp *.tar.gz ticketsharing:/home/ubuntu/projects/objectivescript/repo/${REPO}/modules/
 rm *.tar.gz
 
