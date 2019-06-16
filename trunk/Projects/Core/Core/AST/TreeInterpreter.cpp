fd6a5f9aaafdf45571d295a73de5658ce6685127
madelmann <madelmann@61037b34-3826-4e90-a829-d4c060d010e7>
2019-06-16 14:38:08 +0000

    CORE: slightly refactored the extensions system (more to come)

--- trunk/Projects/Core/Core/AST/TreeInterpreter.cpp
+++ trunk/Projects/Core/Core/AST/TreeInterpreter.cpp
@@ -287,7 +287,7 @@ void TreeInterpreter::evaluateMethodExpression(MethodExpression* exp, Runtime::O
 	assert(method);
 
 	if ( method->isExtensionMethod() ) {
-		mControlFlow = dynamic_cast<ObjectiveScript::ExtensionMethod*>(method)->execute(mThread->getId(), params, result, Token());
+		mControlFlow = dynamic_cast<ObjectiveScript::Extensions::ExtensionMethod*>(method)->execute(mThread->getId(), params, result, Token());
 	}
 	else {
 		mControlFlow = execute(dynamic_cast<Runtime::Object*>(method->getEnclosingScope()), method, params, result);
