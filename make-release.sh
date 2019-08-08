fe5671d0a02b2e546cc5d38207adf8539da52545
madelmann <madelmann@61037b34-3826-4e90-a829-d4c060d010e7>
2019-08-08 12:12:18 +0000

    fixed potential errors in shell scripts

--- make-release.sh
+++ make-release.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 EXCLUDES="--exclude=.* --exclude=cmake-build* --exclude=make-package.cfg"
-RELEASE="ObjectiveScript-`cat version`_`svnversion`.tar.gz"
+RELEASE="ObjectiveScript-$(cat version)_$(svnversion).tar.gz"
 
 echo "Packing files for release ${RELEASE} ..."
 
-tar cfvz ${RELEASE} ${EXCLUDES} *
+# shellcheck disable=SC2086
+tar cfvz "${RELEASE}" ${EXCLUDES} -- *
 
 echo ""
 echo "Uploading files..."
 
-scp ${RELEASE} README.txt madelmann@frs.sourceforge.net:/home/frs/project/objectivescript/
+scp "${RELEASE}" README.txt madelmann@frs.sourceforge.net:/home/frs/project/objectivescript/
 
 echo ""
 echo "Cleanup..."
 
-rm ${RELEASE}
+rm "${RELEASE}"
 
 echo ""
 echo "done."
