fc57732170a2f6641355fb49549fa5ddc7090625
madelmann <madelmann@61037b34-3826-4e90-a829-d4c060d010e7>
2019-09-22 16:38:42 +0000

    Extensions: moved "CreateInstance" to separate "Reflection" sub-extension

--- Projects/Extensions/System/Reflection/ReflectionExtension.cpp
+++ Projects/Extensions/System/Reflection/ReflectionExtension.cpp
@@ -6,6 +6,7 @@
 
 // Project includes
 #include "CreateInstance.h"
+#include "IsKnownType.h"
 
 // Namespace declarations
 
@@ -25,7 +26,7 @@ SystemReflectionExtension::~SystemReflectionExtension()
 {
 }
 
-void SystemReflectionExtension::initialize(IScope* scope)
+void SystemReflectionExtension::initialize(IScope* /*scope*/)
 {
 #ifdef _WIN32
 	// Win32 only
@@ -37,6 +38,7 @@ void SystemReflectionExtension::initialize(IScope* scope)
 void SystemReflectionExtension::provideMethods(ExtensionMethods &methods)
 {
 	methods.push_back(new CreateInstance());
+	methods.push_back(new IsKnownType());
 }
 
 
