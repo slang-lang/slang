c3a11058086ca7cfd84d7461d13925df2462ce81
madelmann <madelmann@61037b34-3826-4e90-a829-d4c060d010e7>
2019-09-22 13:49:43 +0000

    Extensions: added a system extension method that is able to instantiate objects by their type name (var obj = createInstance("String");)

---
+++ Projects/Extensions/System/CreateInstance.h
@@ -0,0 +1,73 @@
+
+#ifndef ObjectiveScript_Extensions_System_CreateInstance_h
+#define ObjectiveScript_Extensions_System_CreateInstance_h
+
+
+// Library includes
+#include <stdlib.h>
+
+// Project includes
+#include <Core/Common/Exceptions.h>
+#include <Core/Designtime/BuildInTypes/StringObject.h>
+#include <Core/Designtime/BuildInTypes/UserObject.h>
+#include <Core/Extensions/ExtensionMethod.h>
+#include <Core/Runtime/BuildInTypes/StringObject.h>
+#include <Core/Runtime/BuildInTypes/UserObject.h>
+#include <Core/Tools.h>
+
+// Forward declarations
+
+// Namespace declarations
+
+
+namespace ObjectiveScript {
+namespace Extensions {
+namespace System {
+
+
+class CreateInstance : public ExtensionMethod
+{
+public:
+	CreateInstance()
+	: ExtensionMethod(0, "createInstance", Designtime::UserObject::TYPENAME)
+	{
+		ParameterList params;
+		params.push_back(Parameter::CreateDesigntime("type", Designtime::StringObject::TYPENAME));
+
+		setSignature(params);
+	}
+
+public:
+	Runtime::ControlFlow::E execute(Common::ThreadId threadId, const ParameterList& params, Runtime::Object* result, const Token& token)
+	{
+		ParameterList list = mergeParameters(params);
+
+		try {
+			ParameterList::const_iterator it = list.begin();
+
+			std::string param_type = (*it++).value().toStdString();
+
+			//Runtime::Object* newInstance = Controller::Instance().repository()->createInstance(param_type, ANONYMOUS_OBJECT);
+			Runtime::Object* newInstance = Controller::Instance().repository()->createReference(param_type, ANONYMOUS_OBJECT, PrototypeConstraints(), Repository::InitilizationType::Final);
+
+			*result = *newInstance;
+		}
+		catch ( std::exception& e ) {
+			Runtime::Object *data = Controller::Instance().repository()->createInstance(Runtime::StringObject::TYPENAME, ANONYMOUS_OBJECT);
+			*data = Runtime::StringObject(std::string(e.what()));
+
+			Controller::Instance().thread(threadId)->exception() = Runtime::ExceptionData(data, token.position());
+			return Runtime::ControlFlow::Throw;
+		}
+
+		return Runtime::ControlFlow::Normal;
+	}
+};
+
+
+}
+}
+}
+
+
+#endif
