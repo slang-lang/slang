d7483434a24edd8dfdc3d5a49acfa353e79a7e1b
madelmann <madelmann@users.noreply.github.com>
2022-12-11 01:13:05 +0100

    added cmake function add_extenal to include externals

--- CMake/Common.cmake
+++ CMake/Common.cmake
@@ -1,4 +1,11 @@
 
+function(add_external path)
+
+        SET(INC_PATH "${path}")
+        include_directories(${INC_PATH})
+
+endfunction()
+
 function(add_include relpath)
 
         set(INC_PATH "${PROJECT_SOURCE_DIR}/${relpath}")
