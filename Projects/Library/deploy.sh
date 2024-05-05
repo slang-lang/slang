2266b538ed91a48b4f915f772274b1fea7741b5d
Michael Adelmann <madelmann@h2953901.stratoserver.net>
2024-05-05 15:52:03 +0200

    bugfix: update module deploy script

--- Projects/Library/deploy.sh
+++ Projects/Library/deploy.sh
@@ -12,9 +12,9 @@ if [ x$2 != "x" ]; then
 	REPO="$2"
 fi
 
-scp *.json ticketsharing:/home/madelmann/projects/slang/repo/${REPO}/modules/
+scp *.json strato1:/home/madelmann/projects/slang-lang.org/repo/${REPO}/modules/
 rm *.json
 
-scp *.tar.gz ticketsharing:/home/madelmann/projects/slang/repo/${REPO}/modules/
+scp *.tar.gz strato1:/home/madelmann/projects/slang-lang.org/repo/${REPO}/modules/
 rm *.tar.gz
 
