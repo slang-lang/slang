6c3b107f6e3d9039d04d288d04c739c85daab840
Michael Adelmann <madelmann@users.noreply.github.com>
2025-01-04 13:21:20 +0100

    - updated version number - minor code cleanup/refactoring

--- Projects/Core/Core/Version.h
+++ Projects/Core/Core/Version.h
@@ -12,9 +12,9 @@
 // Namespace declarations
 
 
-static const char* COPYRIGHT = "Copyright (c) 2014-2024 Michael Adelmann";
+static const char* COPYRIGHT = "Copyright (c) 2014-2025 Michael Adelmann";
 static const char* PRODUCT_NAME = "Slang";
-static const char* PRODUCT_VERSION = "0.8.2";
+static const char* PRODUCT_VERSION = "0.9.0";
 
 
 #endif
