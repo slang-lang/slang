1272f5cb63e379c81a87c416f1e7ec04ab1a1a0a
Michael Adelmann <madelmann@users.noreply.github.com>
2025-01-26 15:56:13 +0100

    - slang-pkg: adapted parts of SemVer v2.0.0 and updated less-comparsion operator to correctly handle version labels and build metadata - slang-pkg: code formatting

--- Projects/slang-pkg/Version.cpp
+++ Projects/slang-pkg/Version.cpp
@@ -10,48 +10,43 @@
 // Namespace declarations
 
 
-SemanticVersionNumber::SemanticVersionNumber()
-: mBugfix(0),
-  mIsValid(false),
-  mMajor(0),
-  mMinor(0)
+SemanticVersionNumber::SemanticVersionNumber( const std::string& version )
 {
+	parse( version );
 }
 
-SemanticVersionNumber::SemanticVersionNumber(const std::string& version)
-: mBugfix(0),
-  mIsValid(false),
-  mMajor(0),
-  mMinor(0)
+SemanticVersionNumber& SemanticVersionNumber::operator=( const std::string& version )
 {
-	parse(version);
-}
-
-SemanticVersionNumber& SemanticVersionNumber::operator=(const std::string& version)
-{
-	parse(version);
+	parse( version );
 
 	return *this;
 }
 
-bool SemanticVersionNumber::operator==(const SemanticVersionNumber& other) const
+bool SemanticVersionNumber::operator==( const SemanticVersionNumber& other ) const
 {
-	return mMajor == other.mMajor && mMinor == other.mMinor && mBugfix == other.mBugfix && mLabel == other.mLabel;
+	return mMajor == other.mMajor
+	    && mMinor == other.mMinor
+	    && mBugfix == other.mBugfix
+	    && mLabel == other.mLabel
+	    && mBuildMetadata == other.mBuildMetadata;
 }
 
-bool SemanticVersionNumber::operator==(const std::string& other) const
+bool SemanticVersionNumber::operator==( const std::string& other ) const
 {
-	SemanticVersionNumber otherVersion(other);
-
-	return operator==(otherVersion);
+	return operator==( SemanticVersionNumber( other ) );
 }
 
-bool SemanticVersionNumber::operator<(const SemanticVersionNumber& other) const
+bool SemanticVersionNumber::operator<( const SemanticVersionNumber& other ) const
 {
 	if ( mMajor == other.mMajor ) {
 		if ( mMinor == other.mMinor ) {
 			if ( mBugfix == other.mBugfix ) {
-				return mLabel < other.mLabel;
+				// Semantic Versioning Specification rule #9
+				if ( !mLabel.empty() && !other.mLabel.empty() ) {
+					return mLabel < other.mLabel;
+				}
+
+				return mLabel.empty();
 			}
 
 			return mBugfix < other.mBugfix;
@@ -68,7 +63,7 @@ bool SemanticVersionNumber::isValid() const
 	return mIsValid;
 }
 
-void SemanticVersionNumber::parse(const std::string& version)
+void SemanticVersionNumber::parse( const std::string& version )
 {
 	mIsValid = false;
 
@@ -77,24 +72,31 @@ void SemanticVersionNumber::parse(const std::string& version)
 	}
 
 	std::string bugfix;
+	std::string buildMetadata;
 	std::string label;
 	std::string major;
 	std::string minor;
 	std::string tmp = version;
 
-	Utils::Tools::splitBy(tmp, '.', major, tmp);
-	Utils::Tools::splitBy(tmp, '.', minor, tmp);
-	Utils::Tools::splitBy(tmp, '-', bugfix, label);
+	Utils::Tools::splitBy( tmp, '.', major, tmp );
+	Utils::Tools::splitBy( tmp, '.', minor, tmp );
+	Utils::Tools::splitBy( tmp, '-', bugfix, tmp );
+	Utils::Tools::splitBy( tmp, '+', label, buildMetadata );
 
-	mMajor = Utils::Tools::stringToInt(major);
-	mMinor = Utils::Tools::stringToInt(minor);
-	mBugfix = Utils::Tools::stringToInt(bugfix);
-	mLabel = label;
+	mBugfix        = Utils::Tools::stringToInt( bugfix );
+	mBuildMetadata = buildMetadata;
+	mLabel         = label;
+	mMajor         = Utils::Tools::stringToInt( major );
+	mMinor         = Utils::Tools::stringToInt( minor );
 
 	mIsValid = mMajor != 0 || mMinor != 0 || mBugfix != 0 || !mLabel.empty();
 }
 
 std::string SemanticVersionNumber::toString() const
 {
-	return Utils::Tools::toString(mMajor) + "." + Utils::Tools::toString(mMinor) + "." + Utils::Tools::toString(mBugfix) + (!mLabel.empty() ? "-" + mLabel : "");
+	return Utils::Tools::toString( mMajor ) + "." +
+	       Utils::Tools::toString( mMinor ) + "." +
+	       Utils::Tools::toString( mBugfix ) +
+	       ( !mLabel.empty() ? "-" + mLabel : "" ) +
+	       ( !mBuildMetadata.empty() ? "+" + mBuildMetadata : "" );
 }
--- Projects/slang-pkg/Version.h
+++ Projects/slang-pkg/Version.h
@@ -16,32 +16,34 @@
 
 /*
  * Represents a comparable version number defined by the defacto versioning standard "semantic versioning"
+ * Refer to https://semver.org/ for details about semantic versioning.
  */
 class SemanticVersionNumber
 {
 public:
-	SemanticVersionNumber();
-	explicit SemanticVersionNumber(const std::string& version);
+	SemanticVersionNumber() = default;
+	explicit SemanticVersionNumber( const std::string& version );
 
 public:
-	SemanticVersionNumber& operator=(const std::string& version);
-	bool operator==(const SemanticVersionNumber& other) const;
-	bool operator==(const std::string& other) const;
-	bool operator<(const SemanticVersionNumber& other) const;
+	SemanticVersionNumber& operator=( const std::string& version );
+	bool operator==( const SemanticVersionNumber& other ) const;
+	bool operator==( const std::string& other ) const;
+	bool operator<( const SemanticVersionNumber& other ) const;
 
 public:
 	bool isValid() const;
 	std::string toString() const;
 
 private:
-	void parse(const std::string& version);
+	void parse( const std::string& version );
 
 private:
-	int mBugfix;
+	int mBugfix{ 0 };
+	std::string mBuildMetadata;
 	std::string mLabel;
-	bool mIsValid;
-	int mMajor;
-	int mMinor;
+	bool mIsValid{ false };
+	int mMajor{ 0 };
+	int mMinor{ 0 };
 };
 
 
